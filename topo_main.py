0816cc44bc43cf45d65e7fd1b36d7d16b68aa7bd
RootFTW <108767514+Root-FTW@users.noreply.github.com>
2023-04-04 18:11:01 -0700

    gtts add

--- topo_main.py
+++ topo_main.py
@@ -1,24 +1,25 @@
 import requests
 from bs4 import BeautifulSoup
+from gtts import gTTS
+from io import BytesIO
 import streamlit as st
 
 # Lista de dominios permitidos
 allowed_domains = ['levelup.com', 'tarreo.com', 'tomatazos.com', 'qore.com', 'sandiegored.com']
 
 # Configurar la página de Streamlit
-st.set_page_config(page_title="Extracción de noticias", page_icon=":newspaper:")
+st.set_page_config(page_title="Extracción y conversión de noticias a audio", page_icon=":newspaper:")
 
 # Título de la página
-st.title("Extracción de noticias")
+st.title("Extracción y conversión de noticias a audio")
 
 # Campo de entrada para ingresar la URL del sitio web
 url = st.text_input("Ingresa la URL del sitio web")
 
 # Verificar si se ha ingresado una URL y si la URL pertenece a la lista de dominios permitidos
 if url and any(domain in url for domain in allowed_domains):
-    # Botón para iniciar la extracción de noticias
-    if st.button("Extraer noticias"):
-
+    # Botón para iniciar la extracción y conversión de noticias
+    if st.button("Extraer y convertir a audio"):
         # Realizar una petición GET a la página web
         response = requests.get(url)
 
@@ -41,6 +42,14 @@ if url and any(domain in url for domain in allowed_domains):
         # Mostrar el título de la noticia y el contenido extraído
         st.header(titulo_noticia)
         st.write(contenido_noticia)
+
+        # Convertir el contenido extraído a habla y guardar el archivo de sonido en un objeto de flujo de bytes
+        sound_file = BytesIO()
+        tts = gTTS(contenido_noticia, lang='es')
+        tts.write_to_fp(sound_file)
+
+        # Reproducir el archivo de sonido en la aplicación web
+        st.audio(sound_file.getvalue(), format='audio/mp3')
 elif url:
     # Mostrar mensaje de error si la URL no pertenece a la lista de dominios permitidos
     st.error("La URL ingresada no pertenece a los dominios permitidos.")
