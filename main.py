1d194556994a550c70aec94f50364b6dc527c6a0
RootFTW <108767514+Root-FTW@users.noreply.github.com>
2023-05-05 16:31:41 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,74 +1,123 @@
 import requests
-from bs4 import BeautifulSoup
 from elevenlabs import generate, set_api_key
 from io import BytesIO
 import streamlit as st
+import feedparser
+from bs4 import BeautifulSoup
 
-allowed_domains = ['levelup.com', 'tarreo.com', 'tomatazos.com', 'qore.com', 'sandiegored.com']
+def should_delete_paragraph(paragraph):
+    phrases_to_ignore = [
+        "Ya llegaron las playeras oficiales de LEVEL UP",
+        "CONSIGUE LA TUYA AQUÍ",
+        "Aquí lo puedes ver",
+        "Editorial:",
+        "Por si te lo perdiste:",
+        "Video relacionado:"
+    ]
 
-st.set_page_config(page_title="Extracción y conversión de noticias a audio", page_icon=":newspaper:")
+    for phrase in phrases_to_ignore:
+        if phrase in paragraph.get_text():
+            return True
+    return False
 
+# Configuración de Streamlit
+st.set_page_config(
+    page_title="Extracción y conversión de noticias a audio", page_icon=":newspaper:"
+)
 st.title("Extracción y conversión de noticias a audio")
 
-url = st.text_input("Ingresa la URL del sitio web")
+# Ingrese la URL del feed RSS 2.0
+rss_url = st.text_input("Ingresa la URL del feed RSS 2.0")
 
-# Aquí el usuario ingresa su API key
+# Ingrese su API key (Elevenlabs)
 api_key = st.text_input("Ingresa tu API key de Elevenlabs")
 
-if url and any(domain in url for domain in allowed_domains):
-    # Botón para analizar la cantidad de caracteres en el texto extraído
-    if st.button("Analizar"):
-        response = requests.get(url)
-        soup = BeautifulSoup(response.content, 'html.parser')
-        
-        titulo_noticia = soup.find('h1', {'itemprop': 'name headline'}).get_text()
-        content = soup.find('div', {'class': 'content', 'id': 'content'})
+if rss_url:
+    feed = feedparser.parse(rss_url)
+    entries = feed.entries[:10]
 
-        contenido_noticia = ""
-        for p in content.find_all('p'):
-            text = p.get_text().lower()
-            if "video relacionado" not in text and "fuente" not in text and "por si te lo perdiste" not in text and "da clic aquí para leer más noticias relacionadas con" not in text and "editorial:" not in text and "entérate:" not in text and "puedes visitar este enlace para conocer todas las noticias relacionadas con" not in text and "busca en este enlace todas las noticias relacionadas con" not in text:
-                contenido_noticia += p.get_text() + "\n\n"
+    for entry in entries:
+        link = entry.link
+        title = entry.title
+        content_encoded = entry.content[0].value
 
-        texto_completo = titulo_noticia + "\n\n" + contenido_noticia
+        # Reemplazar y eliminar <!doctype html> del contenido
+        content_encoded = content_encoded.replace('<!doctype html>', '')
 
-        # Muestra la cantidad total de caracteres en el texto extraído
-        st.write(f"Total de caracteres en el texto: {len(texto_completo)}")
-    # El botón "Extraer y convertir a audio" solo se muestra si el usuario ingresa su clave API
-    if api_key:
-        if st.button("Extraer y convertir a audio"):
-            # Establecer la API key antes de generar el audio
-            set_api_key(api_key)
+        try:
+            p_start = content_encoded.index('<p>')
+            p_end = content_encoded.rindex('</p>') + 4
+            content_encoded = content_encoded[p_start:p_end]
+        except ValueError:
+            pass
 
-            response = requests.get(url)
-            soup = BeautifulSoup(response.content, 'html.parser')
+        content_soup = BeautifulSoup(content_encoded, "html.parser")
 
-            titulo_noticia = soup.find('h1', {'itemprop': 'name headline'}).get_text()
-            content = soup.find('div', {'class': 'content', 'id': 'content'})
+        # Eliminar los atributos "alt" de todas las imágenes
+        for img_tag in content_soup.find_all("img"):
+            img_tag.attrs["alt"] = ""
 
-            contenido_noticia = ""
-            for p in content.find_all('p'):
-                text = p.get_text().lower()
-                if "video relacionado" not in text and "fuente" not in text and "por si te lo perdiste" not in text and "da clic aquí para leer más noticias relacionadas con" not in text and "editorial:" not in text and "entérate:" not in text and "puedes visitar este enlace para conocer todas las noticias relacionadas con" not in text and "busca en este enlace todas las noticias relacionadas con" not in text:
-                    contenido_noticia += p.get_text() + "\n\n"
+        # Eliminar todas las etiquetas <figcaption>
+        for figcaption_tag in content_soup.find_all("figcaption"):
+            figcaption_tag.decompose()
 
-            texto_completo = titulo_noticia + "\n\n" + contenido_noticia
+        # Eliminar todas las etiquetas <blockquote>
+        for blockquote_tag in content_soup.find_all("blockquote"):
+            blockquote_tag.decompose()
 
-            st.header(titulo_noticia)
+        # Eliminar los enlaces con el texto "Fuente"
+        for a_tag in content_soup.find_all("a"):
+            if "Fuente" in a_tag.get_text():
+                a_tag.decompose()
+                
+        # Eliminar todas las etiquetas <a> con atributo data-cb_gallery
+        for a_tag in content_soup.find_all("a", attrs={"data-cb_gallery": True}):
+            a_tag.decompose()
 
-            # Muestra un mensaje de "Cargando..." mientras se genera el audio
-            loading_message = st.empty()
-            loading_message.text("Cargando...")
+        # Eliminar todas las etiquetas <p> que contengan las frases mencionadas
+        for p_tag in content_soup.find_all("p"):
+            if should_delete_paragraph(p_tag):
+                p_tag.decompose()
 
-            audio = generate(text=texto_completo, voice="Arnold", model='eleven_multilingual_v1')
-            sound_file = BytesIO(audio)
+        text_content = content_soup.get_text()
 
-            # Elimina el mensaje de "Cargando..." cuando el audio esté listo
-            loading_message.empty()
+        # Mostrar Título
+        st.write("Título:")
+        st.write(f"**{title}**")
 
-            st.audio(sound_file.getvalue(), format='audio/mp3')
+        # Mostrar URL
+        st.write("URL:")
+        st.write(link)
 
-            st.write(contenido_noticia)
+        # Mostrar Cantidad de Caracteres
+        texto_completo = title + "\n\n" + text_content
+        caracteres = len(texto_completo)
+        st.write("Caracteres:")
+        st.write(caracteres)
 
-elif url:
-    st.error("La URL ingresada no pertenece a los dominios permitidos.")
+        # Mostrar Contenido
+        st.write("Contenido:")
+        st.write(text_content)
+
+        if api_key:
+            if st.button(f"Extraer y convertir a audio - {title}"):
+                # Establecer la API key antes de generar el audio
+                set_api_key(api_key)
+
+                st.header(title)
+
+                # Muestra un mensaje de "Cargando..." mientras se genera el audio
+                loading_message = st.empty()
+                loading_message.text("Cargando...")
+
+                audio = generate(
+                    text=texto_completo, voice="Arnold", model="eleven_multilingual_v1"
+                )
+                sound_file = BytesIO(audio)
+
+                # Elimina el mensaje de "Cargando..." cuando el audio esté listo
+                loading_message.empty()
+
+                st.audio(sound_file.getvalue(), format="audio/mp3")
+
+                st.write(text_content)
